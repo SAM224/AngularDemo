9be50d89b8ee3701aeb8cab3b7048237641ca78b
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-10-27 02:05:05 +0200

    add ngSwitch sample.

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit {
 courses = [];
+viewMode = 'somethinElse';
   constructor() { }
 
   ngOnInit() {
