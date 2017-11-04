75afb5fcaa03d31f78555394bf9dee45a26e0ea3
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-11-04 20:28:58 +0200

    extract error handling method.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,37 +7,36 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-
 @Injectable()
 export class PostService {
   private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: Http) { }
 
-  getPosts = () => this.http.get(this.url);
+  getPosts = () => this.http
+    .get(this.url)
+    .catch(this.handleError);
 
-  createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if (error.status === 400)
-          return Observable.throw(new BadInput(error));
-        return Observable.throw(new AppError(error));
-      });
-  }
+  createPost = (post) => this.http
+    .post(this.url, JSON.stringify(post))
+    .catch(this.handleError);
 
-  updatePost = (post) =>
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
-      .catch((error: Response) => {
-        if (error.status === 404)
-          return Observable.throw(new NotFoundError(error));
-        return Observable.throw(new AppError(error));
-      });
 
-  deletePost(id) {
-    return this.http.delete(this.url + '/' + id)
-      .catch((error: Response) => {
-        if (error.status === 404)
-          return Observable.throw(new NotFoundError(error));
-        return Observable.throw(new AppError(error));
-      });
+  updatePost = (post) => this.http
+    .patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    .catch(this.handleError);
+
+  deletePost = (id) => this.http
+    .delete(this.url + '/' + id)
+    .catch(this.handleError);
+
+
+  private handleError(error: Response) {
+    if (error.status === 400)
+      return Observable.throw(new BadInput(error));
+
+    if (error.status === 404)
+      return Observable.throw(new NotFoundError(error));
+
+    return Observable.throw(new AppError(error));
   }
 }
