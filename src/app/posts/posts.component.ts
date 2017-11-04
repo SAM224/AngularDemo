f875597b68b5916f9f772d550ec64e9661c3ae91
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-11-04 10:43:15 +0200

    Create data.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
   posts: any[];
-
-  constructor(http: Http) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) {
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
+      });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+    
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+        console.log(response.json());
       });
   }
 
