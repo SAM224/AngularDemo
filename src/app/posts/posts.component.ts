808abce2cd5509b28c209e4caa9a102f8f18a831
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-11-04 10:56:06 +0200

    Update data.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -19,13 +19,21 @@ export class PostsComponent implements OnInit {
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
-    
+
     this.http.post(this.url, JSON.stringify(post))
       .subscribe(response => {
         post['id'] = response.json().id;
         this.posts.splice(0, 0, post);
         console.log(response.json());
       });
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+      });
+    //this.http.put(this.url, JSON.stringify(post);
   }
 
   ngOnInit() {
